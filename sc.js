50ab5effbf3530fba4455a3cb783a6081025d884
sanjana200403 <148373335+sanjana200403@users.noreply.github.com>
2024-03-10 19:46:17 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,98 @@
+class Book{
+    constructor(title,author,type){
+        this.title = title
+        this.author = author
+        this.type = type
+        
+        }
+}
+class Display{
+add(book){
+        let uiString =`
+        <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.type}</td>
+        </tr>
+        `
+    tbody.innerHTML += uiString
+    }
+    clear(){
+        let libraryForm= document.getElementById('libraryForm')
+        libraryForm.reset()
+    
+}
+validate(book){
+    if(book.title.length<=2||
+         book.author.length<2){
+        return false
+    }
+    else{
+        return true
+    }
+}
+show(type,message){
+    let msg = document.getElementById('mesaage')
+    msg.innerHTML=`<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+    <strong>Message!</strong> 
+    ${message}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`
+  setTimeout(()=>{
+msg.innerHTML=""
+  },2000)
+
+}
+
+}
+
+// add submit event listeners
+let libraryForm= document.getElementById('libraryForm')
+libraryForm.addEventListener('submit' ,libraryFormSubmit)
+
+
+
+
+function libraryFormSubmit(e){
+    e.preventDefault()
+    let name = document.getElementById('bookName').value
+    let author =document.getElementById('author').value
+    let fiction=document.getElementById('fiction')
+ let programing=document.getElementById('programing')
+ let cooking =document.getElementById('cooking')
+ let type 
+
+ if(
+   fiction .checked){
+type = fiction.value
+ }
+ else if(
+    programing.checked){
+type = programing.value
+ }
+ else if(cooking.checked){
+type =cooking .value
+ }
+ 
+ 
+
+    let book = new Book(name,author,type)
+    console.log(book)
+    console.log(book.title)
+
+    let display = new Display()
+   if( display.validate(book)){
+    display.add(book)
+    display.clear()
+    alert("you have submitted library form")
+    display.show('success',"your book has been  added")
+    
+   }else{
+    // show erroe
+    display.show('danger',"sorry you can not add it")
+   }
+    // display.clear()
+    // console.log("you have submitted library form")
+}
